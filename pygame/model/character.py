74c2d1c129a05cbc8ea2b5410f5facd5169f59cc
marco5de <marco.deuscher@t-online.de>
2020-01-16 21:45:40 +0100

    bisschen stuff fürs model

---
+++ pygame/model/character.py
@@ -0,0 +1,15 @@
+from abc import ABC, ABCMeta, abstractmethod
+
+class Character(ABC):
+    @abstractmethod
+    def __init__(self,bp=0,ap=0,hp=100,ip=0):
+        self.bp = bp
+        self.ap = ap
+        self.hp = hp
+        self.ip = ip
+        self.cocktail = False
+        self.chips = 10
+        self.properties = []
+        self.gadgets = []
+
+
