14c529b5e7dd4904183b9f79cf57c79f1935bb65
marco5de <marco.deuscher@t-online.de>
2020-01-19 03:16:39 +0100

    requirements and configured logging

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 
 
 def init_logging():
-    formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-8.8s]  %(message)s")
+    formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-8.8s] [%(filename)-20.20s - %(funcName)-20.20s - %(lineno)-4.4s] %(message)s")
     rootLogger = logging.getLogger()
 
     fileHandler = logging.FileHandler("out.log")
