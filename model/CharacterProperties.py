af0b8da53ddcb8fe769d04e5acf79b555f3a24c9
Marco Deuscher <33183479+Marco5de@users.noreply.github.com>
2020-03-06 21:17:35 +0100

    Update model/CharacterProperties.py
    Co-Authored-By: csacro <50425802+csacro@users.noreply.github.com>

--- model/CharacterProperties.py
+++ model/CharacterProperties.py
@@ -1,73 +1,21 @@
-from abc import ABC
+from enum import Enum
 
 
-class CharacterProperties(ABC):
-    pass
-
-
-class Nimbleness(CharacterProperties):
-    pass
-
-
-class Sluggishness(CharacterProperties):
-    pass
-
-
-class Spryness(CharacterProperties):
-    pass
-
-
-class Stolidity(CharacterProperties):
-    pass
-
-
-class Agility(CharacterProperties):
-    pass
-
-
-class Jinx(CharacterProperties):
-    pass
-
-
-class LuckyDevil(CharacterProperties):
-    pass
-
-
-class ClammyClothes(CharacterProperties):
-    pass
-
-
-class ConstantClammyClothes(CharacterProperties):
-    pass
-
-
-class RobustStomach(CharacterProperties):
-    pass
-
-
-class Toughness(CharacterProperties):
-    pass
-
-
-class Babysitter(CharacterProperties):
-    pass
-
-
-class HoneyTrap(CharacterProperties):
-    pass
-
-
-class BangAndBurn(CharacterProperties):
-    pass
-
-
-class FlapsAndSeals(CharacterProperties):
-    pass
-
-
-class Tradecraft(CharacterProperties):
-    pass
-
-
-class Observation(CharacterProperties):
-    pass
+class CharacterProperties(Enum):
+    Nimbleness
+    Sluggishness
+    Spryness
+    Stolidity
+    Agility
+    Jinx
+    LuckyDevil
+    ClammyClothes
+    ConstantClammyClothes
+    RobustStomach
+    Toughness
+    Babysitter
+    HoneyTrap
+    BangAndBurn
+    FlapsAndSeals
+    Tradecraft
+    Observation
