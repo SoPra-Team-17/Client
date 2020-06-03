9309044e7e9e7a2f04e0b66d43897a046020ed2e
Marco <marco.deuscher@uni-ulm.de>
2020-06-03 13:23:04 +0200

    highlights active character

--- view/GameView/PlayingFieldScreen.py
+++ view/GameView/PlayingFieldScreen.py
@@ -18,7 +18,6 @@ from network.NetworkEvent import NETWORK_EVENT, NETWORK_EVENT_MESSAGE_TYPES
 __author__ = "Marco Deuscher"
 __date__ = "25.04.2020 (date of doc. creation)"
 
-
 cppyy.add_include_path("/usr/local/include/SopraClient")
 cppyy.add_include_path("/usr/local/include/SopraCommon")
 cppyy.add_include_path("/usr/local/include/SopraNetwork")
@@ -41,6 +40,8 @@ class PlayingFieldScreen(BasicView):
 
         self.background_image = pygame.image.load("assets/GameView/background.png")
         self.background_image = pygame.transform.scale(self.background_image, (1920, 1080))
+
+        self.request_operation = False
 
         self.map = FieldMap(settings)
 
@@ -67,6 +68,8 @@ class PlayingFieldScreen(BasicView):
         if event.type == pygame.USEREVENT and event.user_type == NETWORK_EVENT:
             if event.message_type == "GameStatus":
                 self.update_playingfield()
+            elif event.message_type == "RequestGameOperation":
+                self._update_active_character()
 
     def update_playingfield(self) -> None:
         """
@@ -99,7 +102,6 @@ class PlayingFieldScreen(BasicView):
                 self.map.map[WorldPoint(-1, y, 1)] = Wall(WorldPoint(-1, y, 1), self.asset_storage)
                 self.map.map[WorldPoint(-1, y, 2)] = Wall(WorldPoint(-1, y, 2), self.asset_storage)
 
-        # todo check if coords are right
         for x in range(n_rows):
             for y in range(field_map.getRowLength(x)):
                 # add floor --> I don't think this has to be done on each update! todo
@@ -139,14 +141,22 @@ class PlayingFieldScreen(BasicView):
         for char in state.getCharacters():
             if not char.getCoordinates().has_value():
                 continue
+
+            type = "npc"
+
+            chosen_chars = self.controller.lib_client_handler.lib_client.getChosenCharacters()
+            for chosen_chard_id in chosen_chars:
+                if chosen_chard_id == char.getCharacterId():
+                    type = "my"
+            # todo compare to other lists (enemy etc.)
             point = char.getCoordinates().value()
             self.map.map[WorldPoint(point.x, point.y, z=1)] = Character(WorldPoint(point.x, point.y, z=1),
-                                                                        self.asset_storage)
+                                                                        self.asset_storage, type=type)
         # check if janitor is on playing field
         if state.getJanitorCoordinates().has_value():
             pos_cpp = state.getJanitorCoordinates().value()
-            self.map.map[WorldPoint(pos_cpp.x, pos_cpp.y, z=1)] = Janitor(WorldPoint(pos_cpp.x, pos_cpp.y, z=1),
-                                                                          self.asset_storage)
+            self.map.map[WorldPoint(pos_cpp.x, pos_cpp.y, z=1)] = Character(WorldPoint(pos_cpp.x, pos_cpp.y, z=1),
+                                                                            self.asset_storage, type="janitor")
 
         # check if cat is on playing field
         if state.getCatCoordinates().has_value():
@@ -154,13 +164,12 @@ class PlayingFieldScreen(BasicView):
             self.map.map[WorldPoint(pos_cpp.x, pos_cpp.y, z=1)] = Cat(WorldPoint(pos_cpp.x, pos_cpp.y, z=1),
                                                                       self.asset_storage)
 
-        # mark active char red.
+        logging.info("Successfully updated playing field")
+
+    def _update_active_character(self):
         active_char = self.controller.lib_client_handler.lib_client.getActiveCharacter()
         active_char_coords = self.controller.lib_client_handler.lib_client.getState().getCharacters().findByUUID(
             active_char).getCoordinates().value()
-        wp_coords = WorldPoint(active_char_coords.x, active_char_coords.y, 0)
-        self.map.map[wp_coords].active_char()
-        self.map.map[WorldPoint(active_char_coords.x, active_char_coords.y, 2)] = Floor(
-            WorldPoint(active_char_coords.x, active_char_coords.y, 2), self.asset_storage)
-        self.map.map[WorldPoint(active_char_coords.x, active_char_coords.y, 2)].active_char()
-        logging.info("Successfully updated playing field")
+
+        wp_coords = WorldPoint(active_char_coords.x, active_char_coords.y, z=1)
+        self.map.map[wp_coords] = Character(wp_coords, self.asset_storage, type="my", active=True)
