c1a8d753b397dcfdd3f952051191b09104099898
Marco <marco.deuscher@uni-ulm.de>
2020-06-04 19:10:31 +0200

    restructure assets folder and update paths

---
+++ assets/GameView/CharacterIso/CharacterIsoPaths.py
@@ -0,0 +1,19 @@
+"""
+Includes dict, which holds path to the character iso assets from this folder
+
+Naming convention:
+<dir_name>/<dir_name>
+<dir_name>/<dir_name>_active
+<dir_name>/<dir_name>(_active)(_top/bottom)
+"""
+
+__author__ = "Marco Deuscher"
+__date__ = "03.06.2020 (creation)"
+
+CHARACTER_ISO_PATH_DICT = {
+    "enemy": "assets/GameView/CharacterIso/enemy",
+    "invalid": "assets/GameView/CharacterIso/invalid",
+    "janitor": "assets/GameView/CharacterIso/janitor",
+    "my": "assets/GameView/CharacterIso/my",
+    "npc": "assets/GameView/CharacterIso/npc"
+}
