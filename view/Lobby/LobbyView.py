8bc054aad8379dd55bc656a880976da3e1a8fe78
Marco <marco.deuscher@uni-ulm.de>
2020-06-11 14:46:06 +0200

    dump more info

--- view/Lobby/LobbyView.py
+++ view/Lobby/LobbyView.py
@@ -47,16 +47,24 @@ class LobbyView(BasicView):
 
     def dump_connection_info(self) -> None:
         session_id = self.controller.lib_client_handler.lib_client.getSessionId().to_string_lower()
-        player_id = self.controller.lib_client_handler.lib_client.getId().to_string_lower()
+        client_id = self.controller.lib_client_handler.lib_client.getId().value().to_string_lower()
         name = self.controller.lib_client_handler.lib_client.getName()
+        player_one_id = self.controller.lib_client_handler.lib_client.getPlayerOneId().to_string_lower()
+        player_two_id = self.controller.lib_client_handler.lib_client.getPlayerTwoId().to_string_lower()
+        player_one_name = self.controller.lib_client_handler.lib_client.getPlayerOneName()
+        player_two_name = self.controller.lib_client_handler.lib_client.getPlayerTwoName()
         role = self.controller.lib_client_handler.lib_client.getRole()
         servername = self.settings.address
         port = self.settings.port
 
         json_dict = {}
         json_dict["session_id"] = session_id
-        json_dict["player_id"] = player_id
         json_dict["name"] = name
+        json_dict["client_id"] = client_id
+        json_dict["player_one_id"] = player_one_id
+        json_dict["player_two_id"] = player_two_id
+        json_dict["player_one_name"] = player_one_name
+        json_dict["player_two_name"] = player_two_name
         json_dict["role"] = role
         json_dict["server"] = servername
         json_dict["port"] = port
--- view/MainMenu/MainMenuScreen.py
+++ view/MainMenu/MainMenuScreen.py
@@ -2,18 +2,34 @@
 Implements the actual MainMenu screen
 """
 import logging
+import json
 import pygame_gui.elements.ui_button
 import pygame
+import cppyy
 
 from view.BasicView import BasicView
 from view.ViewSettings import ViewSettings
 from controller.ControllerView import ControllerMainMenu
+from network.NetworkEvent import NETWORK_EVENT
 
 __author__ = "Marco Deuscher"
 __date__ = "25.04.2020 (date of doc. creation)"
 
+cppyy.add_include_path("/usr/local/include/SopraClient")
+cppyy.add_include_path("/usr/local/include/SopraCommon")
+cppyy.add_include_path("/usr/local/include/SopraNetwork")
+
+cppyy.include("util/UUID.hpp")
+cppyy.include("datatypes/character/PropertyEnum.hpp")
+cppyy.include("datatypes/gadgets/GadgetEnum.hpp")
+cppyy.include("datatypes/character/CharacterInformation.hpp")
+cppyy.include("network/messages/MetaInformationKey.hpp")
+cppyy.include("datatypes/character/FactionEnum.hpp")
+cppyy.include("util/GameLogicUtils.hpp")
+
 
 class MainMenuScreen(BasicView):
+    __connection_dump_path = "assets/Connection/connection.json"
 
     def __init__(self, window: pygame.display, controller: ControllerMainMenu, parentView,
                  settings: ViewSettings) -> None:
@@ -37,6 +53,8 @@ class MainMenuScreen(BasicView):
 
         self._init_ui_elements()
 
+        self.__waiting_for_reconnect = False
+
         # load title image
         self.titleImage = pygame.image.load("assets/MainMenu/TitleImage.png")
         logging.info("MainMenuScreen init done")
@@ -56,11 +74,16 @@ class MainMenuScreen(BasicView):
         if event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED:
             switcher = {
                 self.start_game_button: self.start_game_pressed,
+                self.reconnect_button: self.reconnect_pressed,
                 self.help_button: self.help_button_pressed,
                 self.settings_button: self.settings_button_pressed,
                 self.end_game_button: self.controller.exit_game
             }
             switcher.get(event.ui_element)()
+
+        if event.type == pygame.USEREVENT and event.user_type == NETWORK_EVENT:
+            if event.message_type == "GameStatus":
+                self._reconnect_game_status()
 
     def start_game_pressed(self) -> None:
         """
@@ -70,6 +93,43 @@ class MainMenuScreen(BasicView):
         success = self.controller.connect_to_server(self.settings.address, self.settings.port)
         if success:
             self.controller.to_lobby_view()
+
+    def reconnect_pressed(self) -> None:
+        """
+        Reconnect button pressed. Try to restore old game
+        :return:    None
+        """
+        parsed_json = {}
+        try:
+            with open(self.__connection_dump_path, "r") as f:
+                parsed_json = json.load(f)
+        except FileNotFoundError:
+            logging.warning("Connection file does not exist")
+
+        ret = False
+
+        try:
+            session_cpp = cppyy.gbl.spy.util.UUID(parsed_json["session_id"])
+            client_id = cppyy.gbl.spy.util.UUID(parsed_json["client_id"])
+            player_one_id = cppyy.gbl.spy.util.UUID(parsed_json["player_one_ide"])
+            player_two_id = cppyy.gbl.spy.util.UUID(parsed_json["player_two_id"])
+            server_str = parsed_json["server"]
+            port = int(parsed_json["port"])
+            name = parsed_json["name"]
+            player_one_name = parsed_json["player_one_name"]
+            player_two_name = parsed_json["player_two_name"]
+            self.controller.lib_client_handler.lib_client.reconnectPlayerAfterCrash(server_str, port, name, client_id,
+                                                                                    session_cpp, player_one_id,
+                                                                                    player_two_id, player_one_name,
+                                                                                    player_two_name)
+        except KeyError:
+            logging.warning("Json file did not contain needed keys")
+
+        if not ret:
+            return
+
+        self.__waiting_for_reconnect = True
+        logging.info("Successfully reconnected")
 
     def help_button_pressed(self) -> None:
         """
@@ -91,6 +151,15 @@ class MainMenuScreen(BasicView):
                 (self.container.get_size()[0] / 2.75, self.__padding * len(self.container.elements)),
                 self.__buttonSize),
             text="Start Game",
+            manager=self.manager,
+            container=self.container
+        )
+
+        self.reconnect_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(
+                (self.container.get_size()[0] / 2.75, self.__padding * len(self.container.elements)),
+                self.__buttonSize),
+            text="Reconnect",
             manager=self.manager,
             container=self.container
         )
@@ -119,3 +188,6 @@ class MainMenuScreen(BasicView):
             manager=self.manager,
             container=self.container
         )
+
+    def _reconnect_game_status(self) -> None:
+        pass
