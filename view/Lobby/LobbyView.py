d729a3929028e275f047d0e2bd76da57b6fdc9d5
Marco <marco.deuscher@uni-ulm.de>
2020-06-11 14:08:20 +0200

    add connection dump to lobby view

--- view/Lobby/LobbyView.py
+++ view/Lobby/LobbyView.py
@@ -2,6 +2,7 @@
 Implements Lobby View
 """
 import logging
+import json
 import pygame
 
 from view.BasicView import BasicView
@@ -15,6 +16,7 @@ __date__ = "20.05.20 (doc creation)"
 
 
 class LobbyView(BasicView):
+    __connection_dump_path = "assets/Connection/connection.json"
 
     def __init__(self, window: pygame.display, controller: ControllerLobby, settings: ViewSettings) -> None:
         super(LobbyView, self).__init__(window, controller, settings)
@@ -37,7 +39,27 @@ class LobbyView(BasicView):
         if event.type == pygame.USEREVENT and event.user_type == NETWORK_EVENT:
             if event.message_type == "RequestItemChoice":
                 logging.info("Go to Item Choice Phase")
+                self.dump_connection_info()
                 self.controller.to_game_view()
 
         for screen in self.active_screens:
             screen.receive_event(event)
+
+    def dump_connection_info(self) -> None:
+        session_id = self.controller.lib_client_handler.lib_client.getSessionId().to_string_lower()
+        player_id = self.controller.lib_client_handler.lib_client.getId().to_string_lower()
+        name = self.controller.lib_client_handler.lib_client.getName()
+        role = self.controller.lib_client_handler.lib_client.getRole()
+        servername = self.settings.address
+        port = self.settings.port
+
+        json_dict = {}
+        json_dict["session_id"] = session_id
+        json_dict["player_id"] = player_id
+        json_dict["name"] = name
+        json_dict["role"] = role
+        json_dict["server"] = servername
+        json_dict["port"] = port
+
+        with open(self.__connection_dump_path, "w") as f:
+            f.write(json.dumps(json_dict))
